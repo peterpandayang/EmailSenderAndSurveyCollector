d64cd7a8f23fd4a23242fcbdab12f617ab8e1134
peterpandayang <byang14@dons.usfca.edu>
2018-01-08 18:01:17 -0800

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '406398385311-1mqchd1b8390b84didan1dhcscu2betc.apps.googleusercontent.com',
+  googleClientSecret: 'Jw7xxP4CERUfkXUCxFX8y92t',
+  mongoURI: 'mongodb://peterpanda:password@ds245357.mlab.com:45357/emailsenderandsurveycollector',
+  cookieKey:'asdfasdfasdfasdfsdf'
+};
+
+// client id: 406398385311-1mqchd1b8390b84didan1dhcscu2betc.apps.googleusercontent.com
+// client secret: Jw7xxP4CERUfkXUCxFX8y92t
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport'); // make sure that it runs when the server starts. Since we did not want to export this file, we just let it execute
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+app.use(
+  // app is middleware (before sending out to route handler)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// route hanler inside authRoutes
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
